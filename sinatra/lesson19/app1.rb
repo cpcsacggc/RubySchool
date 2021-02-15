0949cb73d247028b0262d97fb7f62e2a4379fcaa
ChurchPC@SACGGC.COM <72246052+cpcsacggc@users.noreply.github.com>
2021-02-15 00:10:14 -0800

    lesson 19 App1 RB

--- sinatra/lesson19/app1.rb
+++ sinatra/lesson19/app1.rb
@@ -1,4 +1,15 @@
 ﻿require "sinatra"
 get '/' do
-	"Hello!"
+	erb :index
+end
+
+post '/' do
+	@login = params[:login]
+	@password = params[:pass]
+	if @login == 'admin' && password == '12345'
+		erb :welcome
+	else
+		@if_stop = "STOP!"
+		erb :index
+	end
 end
