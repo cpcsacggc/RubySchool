ff19fd51eef91f3ad8f1ec55c03f454b6bb90c10
ChurchPC@SACGGC.COM <72246052+cpcsacggc@users.noreply.github.com>
2021-02-15 01:16:35 -0800

    lesson 20 barbershop.rb

---
+++ sinatra/lesson20/barbershop/barbershop.rb
@@ -0,0 +1,23 @@
+﻿require 'sinatra'
+
+get '/' do
+  erb :index
+end
+
+# спросим Имя, номер телефона и дату, когда придёт клиент.
+post '/' do
+  # user_name, phone, date_time
+  @user_name = params[:user_name]
+  @phone = params[:phone]
+  @date_time = params[:date_time]
+
+  @title = "Thank you!"
+  @message = "Уважаемый #{@user_name}, мы ждём вас #{@date_time}"
+
+  # запишем в файл то, что ввёл клиент
+  f = File.open 'users.txt', 'a'
+  f.write "User: #{@user_name}, phone: #{@phone}, date and time: #{@date_time}.\n"
+  f.close
+
+  erb :message
+end
