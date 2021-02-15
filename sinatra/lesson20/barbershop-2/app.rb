3ab2dcee24dd5de7cbd737b9a885466782dd8b7a
ChurchPC@SACGGC.COM <72246052+cpcsacggc@users.noreply.github.com>
2021-02-15 01:24:09 -0800

    lesson 20 b-2 app.rb

---
+++ sinatra/lesson20/barbershop-2/app.rb
@@ -0,0 +1,45 @@
+﻿require 'sinatra'
+
+get '/' do
+  erb :index
+end
+
+# спросим Имя, номер телефона и дату, когда придёт клиент.
+post '/' do
+  # user_name, phone, date_time
+  @user_name = params[:user_name]
+  @phone = params[:phone]
+  @date_time = params[:date_time]
+
+  @title = "Thank you!"
+  @message = "Уважаемый #{@user_name}, мы ждём вас #{@date_time}"
+
+  # запишем в файл то, что ввёл клиент
+  f = File.open 'users.txt', 'a'
+  f.write "User: #{@user_name}, phone: #{@phone}, date and time: #{@date_time}.\n"
+  f.close
+
+  erb :message
+end
+
+# Добавить зону /admin где по паролю будет выдаваться список тех, кто записался (из users.txt)
+
+# sinatra text file sso
+get '/admin' do
+  erb :admin
+end
+
+post '/admin' do
+  @login = params[:login]
+  @password = params[:password]
+
+  # проверим логин и пароль, и пускаем внутрь или нет:
+  if @login == 'admin' && @password == 'krdprog'
+    @file = File.open("./users.txt","r")
+    erb :watch_result
+    # @file.close - должно быть, но тогда не работает. указал в erb
+  else
+    @report = '<p>Доступ запрещён! Неправильный логин или пароль.</p>'
+    erb :admin
+  end
+end
