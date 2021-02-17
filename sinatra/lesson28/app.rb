4bd01b2a5da7a5d23cb975157bdcb3f9fb4fe416
ChurchPC@SACGGC.COM <72246052+cpcsacggc@users.noreply.github.com>
2021-02-17 01:16:54 -0800

    lesson28 init database, make @db

--- sinatra/lesson28/app.rb
+++ sinatra/lesson28/app.rb
@@ -2,7 +2,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
-
+require 'sqlite3'
+def init_db
+	@db = SQLite3::Database.new 'leprosorium.db'
+	@db.results_as_hash = true
+end
+before do
+end
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
