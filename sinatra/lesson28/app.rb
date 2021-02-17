59270ba1ea3ef1bda6bec1958c01d5bebf06caec
ChurchPC@SACGGC.COM <72246052+cpcsacggc@users.noreply.github.com>
2021-02-17 01:22:43 -0800

    lesson28_updated_app.rb

--- sinatra/lesson28/app.rb
+++ sinatra/lesson28/app.rb
@@ -8,6 +8,7 @@ def init_db
 	@db.results_as_hash = true
 end
 before do
+	init_db
 end
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
